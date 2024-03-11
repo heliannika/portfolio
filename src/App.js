a2f037509878162d45c005e7b6d37cab3f2b51c4
heliannika <heliannikar@outlook.com>
2024-03-11 17:00:41 +0200

    hamburger menu added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './styles/styles.css';
 import { useMediaQuery } from 'react-responsive'
 
@@ -5,14 +6,26 @@ function App() {
 
   // Testing what device user is using. Using this to make the site responsive.
 
-  const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'})
-  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  // const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'})
+  // const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
-    <div className="container">
+    <div className="container" 
+        styles={{overflowY: 'scroll'}}>
       {/* Simple menu for navigation. */}
-      <nav>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={showMenu ? 'show' : ''}>
         <ul>
           <li><a href="#">Front page</a></li>
           <li><a href="#">Projects</a></li>
@@ -21,11 +34,11 @@ function App() {
         </ul>
       </nav>
 
-      <div className="deviceTest">
+      {/* <div className="deviceTest">
           {isDesktopOrLaptop && <p>You are using desktop or laptop</p>}
           {isBigScreen && <p>You are using big screen</p>}
           {isTabletOrMobile && <p>You are using a tablet or mobilephone</p>}
-      </div>
+      </div> */}
     
       <div>
         kdfjsdkfkj
